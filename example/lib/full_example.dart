7be71b6b7ce6a212ff3c5b06869f998b3fddc692
Pierre-Louis <plg@google.com>
2021-08-30 10:59:04 +0200

    wip color scheme

---
+++ example/lib/full_example.dart
@@ -0,0 +1,40 @@
+import 'package:dynamic_colors/color_scheme.dart';
+import 'package:dynamic_colors/dynamic_colors_builder.dart';
+import 'package:dynamic_colors/tonal_palette.dart';
+import 'package:flutter/material.dart';
+
+class ExampleApp1 extends StatelessWidget {
+  const ExampleApp1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DynamicColorsBuilder(
+      builder: (TonalPalette? dynamicColors) {
+        // Usually, we might create a ColorScheme from scratch, but here, start from the default light scheme.
+        final ColorScheme lightColorScheme = const ColorScheme.light().copyWith(
+          // On Android S+ devices, use the 500 shade of the primary tonal range.
+          // On other platforms, default to a mid-range amber.
+          primary: dynamicColors?.primary.shade500 ?? Colors.amber[500],
+        );
+        return MaterialApp(
+          // TODO(guidezpl): show how to use with theme
+          theme: ThemeData(
+            /// Harmonize semantic colors, such as [ColorScheme.error], which by default is a dark red.
+            /// Not doing so would cause semantic colors to look out of place next to dynamic colors.
+            colorScheme: lightColorScheme.harmonized(),
+          ),
+          home: Scaffold(
+            body: Container(
+              // On Android S+ devices, use the 500 shade of the primary tonal range.
+              // On other platforms, default to a mid-range amber.
+              color: dynamicColors?.primary.shade500 ?? Colors.amber[500],
+              child: const Center(
+                child: Text('DynamicColorsBuilder example app'),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/color_scheme.dart
@@ -0,0 +1,70 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+///
+int _harmonizeColor(Color from, Color to) {
+  return monet.smartBlend(from.value, to.value, hueP: 0.25, chroma: 0);
+}
+
+extension _ColorHarmonization on Color {
+  Color harmonizeWithPrimary(Color primary) {
+    int harmonizedColor = _harmonizeColor(this, primary);
+    return Color(harmonizedColor);
+  }
+}
+
+extension ColorSchemeHarmonization on ColorScheme {
+  /// Harmonizes specified [ColorScheme] colors with the primary color.
+  ///
+  /// TODO(guidezpl): link to spec
+  ///
+  /// By default, only semantic colors (i.e. colors with meaning) are harmonized.
+  /// Semantic colors are [error] and [onError].
+  ColorScheme harmonized({
+    // No need to harmonize [primary] with itself.
+    bool primaryVariant = false,
+    bool secondary = false,
+    bool secondaryVariant = false,
+    bool surface = false,
+    bool background = false,
+    bool error = true,
+    bool onPrimary = false,
+    bool onSecondary = false,
+    bool onSurface = false,
+    bool onBackground = false,
+    bool onError = true,
+  }) {
+    return copyWith(
+      primaryVariant: !primaryVariant
+          ? this.primaryVariant
+          : this.primaryVariant.harmonizeWithPrimary(primary),
+      secondary: !secondary
+          ? this.secondary
+          : this.secondary.harmonizeWithPrimary(primary),
+      secondaryVariant: !secondaryVariant
+          ? this.secondaryVariant
+          : this.secondaryVariant.harmonizeWithPrimary(primary),
+      surface:
+          !surface ? this.surface : this.surface.harmonizeWithPrimary(primary),
+      background: !background
+          ? this.background
+          : this.background.harmonizeWithPrimary(primary),
+      error: !error ? this.error : this.error.harmonizeWithPrimary(primary),
+      onPrimary: !onPrimary
+          ? this.onPrimary
+          : this.onPrimary.harmonizeWithPrimary(primary),
+      onSecondary: !onSecondary
+          ? this.onSecondary
+          : this.onSecondary.harmonizeWithPrimary(primary),
+      onSurface: !onSurface
+          ? this.onSurface
+          : this.onSurface.harmonizeWithPrimary(primary),
+      onBackground: !onBackground
+          ? this.onBackground
+          : this.onBackground.harmonizeWithPrimary(primary),
+      onError:
+          !onError ? this.onError : this.onError.harmonizeWithPrimary(primary),
+    );
+  }
+}
