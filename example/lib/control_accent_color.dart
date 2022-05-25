b50c05e59512613d0d4386f0809b66872a61e4db
guidezpl <6655696+guidezpl@users.noreply.github.com>
2022-05-25 12:17:34 +0200

    address lint

--- example/lib/control_accent_color.dart
+++ example/lib/control_accent_color.dart
@@ -1,6 +1,6 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:dynamic_color_example/common.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
 
 class ControlAccentColorExample extends StatelessWidget {
