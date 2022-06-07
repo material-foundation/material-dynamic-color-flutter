fd43f33af25f45706c538b81e836e19ed785b509
Craftplacer <craftplacer@craftplacer.moe>
2022-06-07 23:28:24 +0200

    Implement querying DWM window color
    Adds Windows Vista+ support

--- windows/dynamic_color_plugin.cpp
+++ windows/dynamic_color_plugin.cpp
@@ -2,6 +2,7 @@
 
 // This must be included before many other Windows headers.
 #include <windows.h>
+#include <dwmapi.h>
 
 // For getPlatformVersion; remove unless needed for your plugin implementation.
 #include <VersionHelpers.h>
@@ -37,27 +38,60 @@ DynamicColorPlugin::DynamicColorPlugin() {}
 
 DynamicColorPlugin::~DynamicColorPlugin() {}
 
+static BOOL GetAccentColorViaRegistry(int64_t& argbColor) {
+  DWORD abgr = 0;
+  DWORD resultSize = sizeof (abgr);
+
+  if (
+    RegGetValue(
+      HKEY_CURRENT_USER,
+      L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent",
+      L"AccentColorMenu",
+      RRF_RT_REG_DWORD,
+      nullptr,
+      &abgr,
+      &resultSize
+    ) == S_OK
+  ) {
+    argbColor = (abgr & 0xFF00FF00) + ((abgr & 0xFF) << 16) + ((abgr & 0xFF0000) >> 16);
+    return TRUE;
+  }
+
+  return FALSE;
+}
+
+static BOOL GetWindowColorViaDwm(int64_t& argbColor) {
+  DWORD color = 0;
+  BOOL opaque = FALSE;
+
+  if (SUCCEEDED(DwmGetColorizationColor(&color, &opaque))) {
+    argbColor = color;
+    return TRUE;
+  }
+
+  return FALSE;
+}
+
 void DynamicColorPlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue> &method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
   if (!method_call.method_name().compare("getAccentColor")) {
-    int64_t argbColor = 0;
-    DWORD abgr = 0;
-    DWORD resultSize = sizeof (abgr);
-    if (
-      RegGetValue(
-        HKEY_CURRENT_USER,
-        L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent",
-        L"AccentColorMenu",
-        RRF_RT_REG_DWORD,
-        nullptr,
-        &abgr,
-        &resultSize
-      ) == S_OK
-    ) {
-      argbColor = (abgr & 0xFF00FF00) + ((abgr & 0xFF) << 16) + ((abgr & 0xFF0000) >> 16);
-      result->Success(flutter::EncodableValue(argbColor));
-    } else result->Success(nullptr);
+    int64_t argb = 0;
+    BOOL successful = FALSE;
+
+    // Try one method after the other until one works
+    if (IsWindows10OrGreater()) {
+      successful = GetAccentColorViaRegistry(argb);
+    }
+    if (!successful && IsWindowsVistaOrGreater()) {
+      successful = GetWindowColorViaDwm(argb);
+    }
+
+    if (successful) {
+      result->Success(flutter::EncodableValue(argb));
+    } else {
+      result->Success(nullptr);
+    }
   } else {
     result->NotImplemented();
   }
